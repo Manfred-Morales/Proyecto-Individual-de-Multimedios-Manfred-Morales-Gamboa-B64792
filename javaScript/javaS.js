ee9da8d28b4121a4e6c72577cff3573ea0e23cd6
Manfred david morales gamboa <manfred1998m@gmail.com>
2020-07-11 22:03:03 -0600

    primer commit

---
+++ javaScript/javaS.js
@@ -0,0 +1,13 @@
+var listaimg = ["img/foto 1.jpg", "img/foto 2.jpg", "img/foto 3.jpg","img/foto 4.jpg"];
+
+var x=0;
+
+function cambioImagenes(){
+    if (x > 3){
+        x=0;
+    }
+    document.getElementById("imgPersonal").src = listaimg[x];
+    x++;
+}
+
+temporizador =setInterval("cambioImagenes()", 2000);
